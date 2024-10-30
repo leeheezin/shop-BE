6d009dcb5439a3d3f6bb54488edc5a3714bfe1a6
heeezin <gmlwls839@nate.com>
2024-10-31 00:59:48 +0900

    검색,페이지네이션

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
+const { response } = require("express")
 const Product = require("../model/Product")
-
+const PAGE_SIZE=1
 const productController = {}
 productController.createProduct = async (req,res) => {
     try {
@@ -13,8 +14,21 @@ productController.createProduct = async (req,res) => {
 }
 productController.getProducts = async (req,res) => {
     try {
-        const products = await Product.find({})
-        res.status(200).json({status:'success',data:products})
+        const {page,name} = req.query
+        const cond = name?{name:{$regex:name,$options:'i'}}:{}
+        let query = Product.find(cond)
+        if(page){
+            query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE)
+            //최종 몇개 페이지
+            //데이터가 총 몇개 있는지
+            const totalItemNum = await Product.countDocuments(cond)
+            //데이터 총 개수/ PAGE_SIZE 
+            const totalPageNum = Math.ceil(totalItemNum/PAGE_SIZE)
+            response.totalPageNum=totalPageNum
+        }
+        const productList = await query.exec()
+        response.data=productList
+        res.status(200).json(response)
     } catch (error) {
         res.status(400).json({status:'fail', error: error.message})
     }
