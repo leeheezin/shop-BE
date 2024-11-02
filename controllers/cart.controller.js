89feac3c0ef95b08a97a7e2ddd3e109cf2fae760
heeezin <gmlwls839@nate.com>
2024-11-03 00:17:58 +0900

    카트페이지

---
+++ controllers/cart.controller.js
@@ -0,0 +1,44 @@
+const Cart = require("../model/Cart")
+
+const cartController = {}
+cartController.addItemToCart = async (req,res) =>{
+    try {
+        const {userId} = req
+        const {productId,size,qty} = req.body
+        // 유저를 가지고 카트 찾기
+        let cart = await Cart.findOne({userId})
+        // 유저가 만든 카트가 없다->만들어주기
+        if(!cart) {
+            cart = new Cart({userId})
+            await cart.save()
+        }
+        // 이미 카트에 들어가있는 아이템이냐? productId
+        const existItem = cart.items.find((item)=>item.productId.equals(productId)&& item.size === size)
+        // 그렇다면 에러 ('이미 아이템이 카트에 있습니다')
+        if(existItem){
+            throw new Error("이미 아이템이 카트에 있습니다.")
+        }
+        // 카트에 아이템을 추가
+        cart.items = [...cart.items,{productId,size,qty}]
+        await cart.save()
+        res.status(200).json({status:"success", data: cart, cartItemQty: cart.items.length})
+    } catch (error) {
+        res.status(400).json({status:"fail",error: error.message})
+    }
+}
+cartController.getItemCart = async (req,res) => {
+    try {
+        const {userId} = req
+        const cart = await Cart.findOne({userId}).populate({
+            path:'items',
+            populate:{
+                path: 'productId',
+                model: 'Product'
+            }
+        })
+        res.status(200).json({status: "success",data: cart.items})
+    } catch (error) {
+        res.status(400).json({status: "fail",error: error.message})
+    }
+}
+module.exports = cartController
